a042dcb587ff3bb3981caf3bb7f28009d6cfab60
09bre <brenda.silva.maciel@escola.pr.gov.br>
2025-10-02 10:54:01 -0300

    .

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ const botaoMostraPalavras = document.quelySeletor('#botaopalavrachave');
 botaoMostraPalvras.addEventListener('click', mostraPalvraChave);
 
 function mostraPalvrasChave (){
-    alert('Fui clicado!');
+    const texto = document.querySelector('#entrada-de-texto'). value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave = texto.split(" ");
+
+    campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto){
+    let palavras = texto.split(" ");
+    return palavras;
 }
